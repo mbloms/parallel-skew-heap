39bd1e1f0b7d767f3c628f3c6272d6f39e924526
Mikael Blomstrand <mbloms@kth.se>
2019-05-24 02:05:58 +0200

    simple heapsort

--- skewheap.hs
+++ skewheap.hs
@@ -1,4 +1,5 @@
 import Control.Parallel
+import Data.List (foldl', unfoldr)
 
 data SkewHeap a = Empty
                 | Node a (SkewHeap a) (SkewHeap a)
@@ -17,9 +18,20 @@ merge t1@(Node x1 l1 r1) t2@(Node x2 l2 r2)
 insert :: Ord a => a -> SkewHeap a -> SkewHeap a
 insert x heap = singleton x `merge` heap
 
-peak :: Ord a => SkewHeap a -> a
-peak (Node x _ _) = x
+peak :: Ord a => SkewHeap a -> Maybe a
+peak (Node x _ _) = Just x
+peak Empty = Nothing
 
-pop :: Ord a => SkewHeap a -> SkewHeap a
-pop Empty = Empty
-pop (Node _ l r) = merge l r
+delete_min :: Ord a => SkewHeap a -> Maybe (SkewHeap a)
+delete_min (Node _ l r) = Just $ merge l r
+delete_min Empty = Nothing
+
+pop :: Ord a => SkewHeap a -> Maybe (a, SkewHeap a)
+pop heap = do
+    min <- peak heap
+    rest <- delete_min heap
+    return (min, rest)
+
+heapsort :: Ord a => [a] -> [a]
+heapsort = unfoldr pop . foldl' (flip insert) Empty
+
